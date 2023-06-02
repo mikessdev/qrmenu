8f60f1c9724f15216a06b8a88dbfa7a830b78858
Erick Santana <120190518+ErickDevp@users.noreply.github.com>
2023-06-01 23:12:20 -0300

    Criando componente Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,15 @@
 <template>
-    <p>Header</p>
+    <header>
+        <p>Buteco Piece</p>
+    </header>
 </template>
+<style lang="scss" scoped>
+    header {
+        background: #F5003B;
+        color: #fff;
+        font-family: 'Noto Sans';
+        font-weight: bold;
+        text-align: center;
+        padding: 1rem 1rem;
+    }
+</style>
