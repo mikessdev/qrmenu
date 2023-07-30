53e5f877e0494c9995cf7592b4095602d2dcdc02
mikessdev <mikesaints.2000@gmail.com>
2023-07-29 22:50:37 -0300

    adding .env file and login with firebase

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -4,7 +4,7 @@ import { ref } from 'vue';
 //TODO Implement communication with the backend when the backend is available
 
 export const useUserStore = defineStore('userProfile', () => { 
-    const isAdmin = ref(true); 
+    const isAdmin = ref(false); 
     return {
         isAdmin,
     }
