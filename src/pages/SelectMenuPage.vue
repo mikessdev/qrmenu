186e06dd8ff8ac512b0ffe333089c49b67a11c81
mikessdev <mikess.dev@gmail.com>
2023-11-14 18:34:54 -0300

    feat: creating select-menu page

--- src/pages/SelectMenuPage.vue
+++ src/pages/SelectMenuPage.vue
@@ -1,17 +1,21 @@
 <script setup lang="ts">
 import Header from '@/components/Header.vue';
-import Hero from '@/components/Hero.vue';
-import HorizontalMenu from '@/components/HorizontalMenu.vue';
-import { ref } from 'vue';
-
-const background = ref('bg-[#67177b]');
+import PlusIcon from '@/components/icons/PlusIcon.vue';
 </script>
 
 <template>
-  <Header :center="true" color="#67177b" />
-  <main>
-    <Hero />
-    <HorizontalMenu />
+  <Header :center="true" />
+  <main class="flex flex-col items-center pt-[60px]">
+    <p class="mb-[60px] font-notosans text-xl text-[#4E4E4E]">
+      Selecione o botão abaixo para criar um novo cardápio.
+    </p>
+    <router-link to="/create-menu">
+      <div
+        class="flex h-[100px] w-[100px] cursor-pointer items-center justify-center rounded-full border-[4px] border-[#DCDCDC] bg-qr-medium-gray"
+      >
+        <PlusIcon variante="02" :width="26" :height="26" color="#DCDCDC" />
+      </div>
+    </router-link>
   </main>
 </template>
 
