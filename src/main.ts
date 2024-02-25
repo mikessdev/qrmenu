b032fc88aec4e87b9346b69d404d6ef76c9baa5f
mikessdev <mikesaints.2000@gmail.com>
2024-02-25 15:26:29 -0300

    feat: add quasar framework

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import App from './App.vue';
 import router from './router';
 import './assets/css/main.css';
 import { VueRecaptchaPlugin } from 'vue-recaptcha/head';
+import { Quasar } from 'quasar';
+import quasarUserOptions from './quasar-user-options';
 
 const pinia = createPinia();
 const app = createApp(App);
@@ -11,6 +13,8 @@ const app = createApp(App);
 app.use(VueRecaptchaPlugin, {
   v2SiteKey: import.meta.env.VITE_RECAPTCHA_SITE_KEY
 });
+
+app.use(Quasar, quasarUserOptions);
 app.use(router);
 app.use(pinia);
 app.mount('#app');
