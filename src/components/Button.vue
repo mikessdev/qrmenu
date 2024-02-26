3d1f4ed94a032a094b3426063ad762d9dcae36c0
mikessdev <mikesaints.2000@gmail.com>
2024-02-25 22:09:47 -0300

    style: fix inconsistent button css

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -40,8 +40,8 @@ const props = defineProps({
       v-if="props.variante === 'secundary'"
       :class="[
         props.isDisabled
-          ? 'button-font h-[48px] cursor-pointer rounded-[10px] bg-[#9ea3a0] px-[16px] text-white shadow-[0_2px_4px_rgb(192,194,193,0.4),0_7px_13px_-3px_rgb(192,194,193,0.3),0_-3px_0_inset_#c0c2c1]  focus-visible:outline-none'
-          : 'button-font h-[48px] cursor-pointer rounded-[10px] bg-qr-primary-orange px-[16px] text-white shadow-[0_2px_4px_rgb(246,147,124,0.4),0_7px_13px_-3px_rgb(246,147,124,0.3),0_-3px_0_inset_#f6937c] transition-transform duration-150 hover:-translate-y-[2px] hover:shadow-[0_4px_8px_rgb(246,147,124,0.4),0_7px_13px_-3px_rgb(246,147,124,0.3),0_-3px_0_inset_#f6937c] focus-visible:outline-none active:-translate-y-[-2px] active:shadow-[0_3px_7px_inset_#f6937c]'
+          ? 'button-font h-[48px] cursor-pointer rounded-[10px] bg-[#9ea3a0] px-[16px] text-white shadow-[0_2px_4px_rgb(45,35,66,0.4),0_7px_13px_-3px_rgb(45,35,66,0.3),0_-3px_0_inset_#c0c2c1]  focus-visible:outline-none'
+          : 'button-font h-[48px] cursor-pointer rounded-[10px] bg-qr-primary-orange px-[16px] text-white shadow-[0_2px_4px_rgb(45,35,66,0.4),0_7px_13px_-3px_rgb(45,35,66,0.3),0_-3px_0_inset_#f6937c] transition-transform duration-150 hover:-translate-y-[2px] hover:shadow-[0_4px_8px_rgb(45,35,66,0.4),0_7px_13px_-3px_rgb(45,35,66,0.3),0_-3px_0_inset_#f6937c] focus-visible:outline-none active:-translate-y-[-2px] active:shadow-[0_3px_7px_inset_#f6937c]'
       ]"
       :type="props.type"
     >
