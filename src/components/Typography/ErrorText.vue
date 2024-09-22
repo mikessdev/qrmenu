2762254c99ae066cf12c81141f82ec3923e7139d
mikessdev <mikesaints.2000@gmail.com>
2024-09-22 12:14:45 -0300

    feat: :sparkles: Create typography component

---
+++ src/components/Typography/ErrorText.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { PropType } from 'vue';
+
+const props = defineProps({
+  props: {
+    type: Object as PropType<Record<string, any>>,
+    default: () => ({})
+  },
+  text: {
+    type: String,
+    default: ''
+  }
+});
+</script>
+
+<template>
+  <div class="text-caption text-error" v-bind:props="props">
+    {{ text }}
+  </div>
+</template>
---
+++ src/components/Typography/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { PropType } from 'vue';
+
+const props = defineProps({
+  props: {
+    type: Object as PropType<Record<string, any>>,
+    default: () => ({})
+  },
+  text: {
+    type: String,
+    default: ''
+  }
+});
+</script>
+<template>
+  <div :props="props">{{ text }}</div>
+</template>
